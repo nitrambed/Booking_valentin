3cdea34806cef3d23c065c5a6527a8e29284069f
Deborah MARTIN <deb.martin@live.fr>
2022-02-21 17:13:22 +0100

    Modified validates on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,8 @@ class User < ApplicationRecord
   has_many :valentins, dependent: :destroy
   has_many :reviews, dependent: :destroy
   has_many :bookings, dependent: :destroy
+
+  validates :first_name, presence: true, length: { minimum: 2 }
+  validates :last_name, presence: true, length: { minimum: 2 }
+  validates :email, presence: true, uniqueness: true
 end
