800b79ed79ad2d39c12c3de4bdad024e189cd6ca
Deborah MARTIN <deb.martin@live.fr>
2022-02-22 11:23:27 +0100

    Fixed problems with model validations for Valentin and Booking

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,8 +2,5 @@ class Booking < ApplicationRecord
   belongs_to :valentin
   belongs_to :user
 
-  validates :name, presence: true, length: { minimum: 8 }
-  validates :price, presence: true, numericality: { only_integer: true }
-  validates :short_description, presence: true, length: { maximum: 500 }
-  validates :long_description, presence: true, length: { maximum: 5000 }
+  validates :reservation_at
 end
--- app/models/valentin.rb
+++ app/models/valentin.rb
@@ -1,3 +1,8 @@
 class Valentin < ApplicationRecord
   belongs_to :user
+
+  validates :name, presence: true, length: { minimum: 8 }
+  validates :price, presence: true, numericality: { only_integer: true }
+  validates :short_description, presence: true, length: { maximum: 500 }
+  validates :long_description, presence: true, length: { maximum: 5000 }
 end
