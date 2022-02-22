bf3f3cf29201a06b447a7469c9fae4a997f2ec47
Deborah MARTIN <deb.martin@live.fr>
2022-02-22 15:33:19 +0100

    Added a seed file, booking controller and views to test

--- config/routes.rb
+++ config/routes.rb
@@ -9,11 +9,11 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :valentins, only: [:index, :show, :new, :create, :destroy]
-  
+
   resources :valentins do
     resources :reviews, only: [:index, :new]
     resources :bookings, only: [:new]
   end
 
-  resources :bookings, only: [:index, :edit, :destroy]
+  resources :bookings, only: [:index, :show, :create, :edit, :update, :destroy]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,70 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Creating seeds...'
+
+  user2 = User.new(
+    first_name: "User",
+    last_name: "Test",
+    email: "user@aaaaaazeaz.fr",
+    password: "coucou"
+  )
+  user2.save!
+
+  valentin = Valentin.new(
+    name:    "Valentino",
+    price: 20,
+    user_id: 1,
+    short_description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    long_description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+
+  )
+  valentin.save!
+
+
+  valentina = Valentin.new(
+    name:    "Valentina",
+    price: 30,
+    user_id: 2,
+    short_description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    long_description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+
+  )
+  valentina.save!
+
+
+  val = Valentin.new(
+    name:    "Le Val de l'enfer",
+    price: 300,
+    user_id: 1,
+    short_description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    long_description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+
+  )
+  val.save!
+
+  booking = Booking.new(
+    user_id: 1,
+    valentin_id: 3,
+    reservation_at: "01/01/2021"
+  )
+  booking.save!
+
+
+  booking1 = Booking.new(
+    user_id: 2,
+    valentin_id: 1,
+    reservation_at: "01/01/2021"
+  )
+  booking1.save!
+
+
+  booking2 = Booking.new(
+    user_id: 1,
+    valentin_id: 2,
+    reservation_at: "01/01/2021"
+  )
+  booking2.save!
+
+puts 'Finished!'
